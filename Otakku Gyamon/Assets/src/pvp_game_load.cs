14ee900e9c9264683a76c0a2a9a6d4233dc72357
Ogura Deko <fanta51@live.jp>
2021-01-20 11:09:07 +0900

    base of initial var, diceroll and sequential order comp.

---
+++ Otakku Gyamon/Assets/src/pvp_game_load.cs
@@ -0,0 +1,236 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class pvp_game_load : MonoBehaviour
+{
+    bool user = false;  //プレイヤー(左側がtrue)
+    bool gameready = false; //ready to game
+    bool ongame = false;    //playing game
+    bool canroll = false;   //ダイスロールの可否
+    int turn = 0;   //経過ターン
+    int dice = 0;   //ダイスの目
+    int remain = 0; //残りの移動回数
+    const int komakazu = 7; //コマの数
+    int[] lkoma;    //左側コマの位置
+    int[] rkoma;    //右側コマの位置
+    int[] ground;   //フィールドのコマの数
+    int roll1 = 0, roll2 = 0;
+
+    public GameObject lkoma0_obj;
+    public GameObject lkoma1_obj;
+    public GameObject lkoma2_obj;
+    public GameObject lkoma3_obj;
+    public GameObject lkoma4_obj;
+    public GameObject lkoma5_obj;
+    public GameObject lkoma6_obj;
+    public GameObject lkoma7_obj;
+    public GameObject rkoma0_obj;
+    public GameObject rkoma1_obj;
+    public GameObject rkoma2_obj;
+    public GameObject rkoma3_obj;
+    public GameObject rkoma4_obj;
+    public GameObject rkoma5_obj;
+    public GameObject rkoma6_obj;
+    public GameObject rkoma7_obj;
+    public GameObject diceview1_obj;
+    public GameObject diceview2_obj;
+    public GameObject diceview3_obj;
+    public GameObject diceview4_obj;
+    public Material di1;
+    public Material di2;
+    public Material di3;
+    public Text turntext;
+    public Text todotext;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("[Function (Enter)] void Start()");
+        /* フィールド初期化 */
+        ground = new int[16];
+        for (int i = 0; i < 16; i++)
+        {
+            ground[i] = new int();
+            switch (i)
+            {
+                case 2:
+                    ground[i] = 2;
+                    break;
+                case 4:
+                    ground[i] = -3;
+                    break;
+                case 7:
+                    ground[i] = 3;
+                    break;
+                case 8:
+                    ground[i] = -3;
+                    break;
+                case 11:
+                    ground[i] = 3;
+                    break;
+                case 13:
+                    ground[i] = -2;
+                    break;
+                default:
+                    ground[i] = 0;
+                    break;
+            }
+        }
+
+        /*
+        【補足】
+        たとえば、Left Playerがground[2]に3体いるならば、"ground[2] = -3" になり、
+        RightPlayerがground[6]に1体ならば、"ground[6] = 1" となる。
+
+        【値早見表】
+        ground[0] = 右側ゴール (Left Cassle)
+              [1] = 右側島流し (Right側のコマが監禁)
+              [2] = フィールド1 (Left側陣地@1)
+              [3] = フィールド2 (Left側陣地@2)
+              [4] = フィールド3 (Left側陣地@3)
+              [5] = フィールド4 (Mid陣地Left側@1)
+              [6] = フィールド5 (Mid陣地Left側@2)
+              [7] = フィールド6 (Mid陣地Left側@3)
+              [8] = フィールド7 (Mid陣地Right側@3)
+              [9] = フィールド8 (Mid陣地Right側@2)
+              [10] = フィールド9 (Mid陣地Right側@1)
+              [11] = フィールド10 (Right側陣地@3)
+              [12] = フィールド11 (Right側陣地@2)
+              [13] = フィールド12 (Right側陣地@1)
+              [14] = 左側島流し (Left側のコマが監禁)
+              [15] = 左側ゴール (Right Cassle)
+    */
+
+        /* コマ初期化 */
+        lkoma = new int[8];
+        rkoma = new int[8];
+        lkoma[0] = new int();
+        lkoma[0] = 2;
+        lkoma[1] = new int();
+        lkoma[1] = 2;
+        rkoma[0] = new int();
+        rkoma[0] = 4;
+        rkoma[1] = new int();
+        rkoma[1] = 4;
+        rkoma[2] = new int();
+        rkoma[2] = 4;
+        lkoma[2] = new int();
+        lkoma[2] = 7;
+        lkoma[3] = new int();
+        lkoma[3] = 7;
+        lkoma[4] = new int();
+        lkoma[4] = 7;
+        rkoma[3] = new int();
+        rkoma[3] = 8;
+        rkoma[4] = new int();
+        rkoma[4] = 8;
+        rkoma[5] = new int();
+        rkoma[5] = 8;
+        lkoma[5] = new int();
+        lkoma[5] = 11;
+        lkoma[6] = new int();
+        lkoma[6] = 11;
+        lkoma[7] = new int();
+        lkoma[7] = 11;
+        rkoma[6] = new int();
+        rkoma[6] = 13;
+        rkoma[7] = new int();
+        rkoma[7] = 13;
+
+        /* Ready! */
+        turn = 0;   //ターン数初期化
+        dice = 0;   //ダイスの値初期化
+        remain = 0; //残りダイス数初期化
+        user = false;
+        ongame = false;
+        canroll = false;
+        gameready = true;
+        Debug.Log("[Function (Finish)] void Start()");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("[Function (Enter)] void Update()");
+        if (ongame)
+        {
+            Debug.Log("[Sub] ongame");
+            //playing game
+            if (canroll)
+            {
+                Debug.Log("[Sub] canroll");
+                if (Input.GetKeyDown(KeyCode.D))
+                {
+                    Debug.Log("[Sub] keydown(D)");
+                    //dice roll
+                    canroll = false;
+                    turn++;
+                    turntext.text = "Turn: " + turn.ToString();
+                    canroll = true;
+                }
+            }
+        }else if (gameready)
+        {
+            Debug.Log("[Sub] gameready");
+            //順番決め
+            todotext.text = "先攻を決めています…";
+            do
+            {
+                roll1 = diceroll();
+                switch (roll1)
+                {
+                    case 0:
+                            diceview1_obj.GetComponent<Image>().material = di1;
+                        break;
+                    case 1:
+                            diceview1_obj.GetComponent<Image>().material = di2;
+                        break;
+                    default:
+                            diceview1_obj.GetComponent<Image>().material = di3;
+                        break;
+                }
+
+                roll2 = diceroll();
+                switch (roll2)
+                {
+                    case 0:
+                            diceview2_obj.GetComponent<Image>().material = di1;
+                        break;
+                    case 1:
+                            diceview2_obj.GetComponent<Image>().material = di2;
+                        break;
+                    default:
+                            diceview2_obj.GetComponent<Image>().material = di3;
+                        break;
+                }
+            } while (roll1 == roll2);
+
+            if(roll1 > roll2)
+            {
+                user = true;
+                todotext.text = "←　先攻が決まりました！";
+            }
+            else
+            {
+                user = false;
+                todotext.text = "先攻が決まりました！　→";
+            }
+
+            canroll = true;
+            ongame = true;
+            gameready = false;
+        }
+    }
+
+    int diceroll()
+    {
+        Debug.Log("[Function (Enter)] int diceroll()");
+        int diceval;
+        diceval = Random.Range(0, 3);
+        return diceval;
+    }
+
+}
